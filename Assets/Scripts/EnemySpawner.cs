c53fd9eae3fa7fe427396d269fbab25b801c9d1d
Century178 <mrothenberger0918@gmail.com>
2023-02-18 19:59:00 -0700

    Spawners

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject enemy;
+
+    [SerializeField] private float range;
+    [SerializeField] private float time;
+    [SerializeField] private float maxTime;
+
+    [SerializeField] private bool spawn;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        time = maxTime;
+        spawn = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time -= Time.deltaTime;
+
+        if (time <= 0 && spawn)
+        {
+            Instantiate(enemy, new Vector3(transform.position.x + Random.Range(-range, range), transform.position.y, transform.position.z), Quaternion.identity);
+            time = maxTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        spawn = false;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        spawn = true;
     }
 }
