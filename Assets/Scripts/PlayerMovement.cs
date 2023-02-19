0739e425221e775b8cd0c2b1bc38758595212392
Jason Chan <shunsei.chan@gmail.com>
2023-02-18 18:23:22 -0800

    Player sturff

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float x, y;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (x == 0 && y == 0) return;
+        rb.velocity = new Vector2(x, y).normalized * speed;
+    }
+}
