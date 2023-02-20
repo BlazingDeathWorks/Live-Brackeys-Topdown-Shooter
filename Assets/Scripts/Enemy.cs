15ef0fab0eefcacd86e3d69eb6924d203dfa7768
Century178 <mrothenberger0918@gmail.com>
2023-02-19 20:06:58 -0700

    Broken enemy looking

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,14 @@
+using Mono.CompilerServices.SymbolWriter;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float offset = 90f;
 
     void Awake()
     {
@@ -16,6 +20,9 @@ public class Enemy : MonoBehaviour
         if (PlayerMovement.player != null)
         {
             transform.position = Vector2.MoveTowards(transform.position, PlayerMovement.player.transform.position, (moveSpeed + GameManager.difficulty) * Time.deltaTime);
+
+            Vector3 rawDirection = transform.position - PlayerMovement.player.transform.position;
+            transform.localEulerAngles = new Vector3(0, 0, (Mathf.Atan2(rawDirection.y, rawDirection.x * Mathf.Rad2Deg) - offset));
         }
     }
 }
