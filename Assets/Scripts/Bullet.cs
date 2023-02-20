9715aafa09609c80f1f458587a1d47708323d7c5
Century178 <mrothenberger0918@gmail.com>
2023-02-19 19:34:28 -0700

    Enemy death and difficulty adjustment

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -24,6 +24,11 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
+
         Destroy(gameObject);
     }
 }
