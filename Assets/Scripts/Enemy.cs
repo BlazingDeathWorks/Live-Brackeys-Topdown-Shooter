fc46d7902342bb995f4113e50aab1763b85d523d
Jason Chan <shunsei.chan@gmail.com>
2023-02-19 19:17:00 -0800

    emfe

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,11 +10,6 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] private float offset = 90f;
 
-    void Awake()
-    {
-        
-    }
-
     void Update()
     {
         if (PlayerMovement.player != null)
@@ -22,7 +17,7 @@ public class Enemy : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, PlayerMovement.player.transform.position, (moveSpeed + GameManager.difficulty) * Time.deltaTime);
 
             Vector3 rawDirection = transform.position - PlayerMovement.player.transform.position;
-            transform.localEulerAngles = new Vector3(0, 0, (Mathf.Atan2(rawDirection.y, rawDirection.x * Mathf.Rad2Deg) - offset));
+            transform.localEulerAngles = new Vector3(0, 0, (Mathf.Atan2(rawDirection.y, rawDirection.x) * Mathf.Rad2Deg) - offset);
         }
     }
 }
